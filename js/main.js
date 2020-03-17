334f754169083ab2d2136a7afd61377780b90346
Alexander Kirienkov <shurakirienkov@yandex.ru>
2020-03-17 21:59:49 +0300

    feat: _control.sass and modal

---
+++ js/main.js
@@ -0,0 +1,58 @@
+/*
+document.addEventListener("DOMContentLoaded", function(event) {
+    const modal = document.querySelector('.modal');
+    const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+    const closeBtn = document.querySelector('.modal__close');
+    const switchModal = () => {
+        modal.classList.toggle('modal--visible');
+    }
+    modalBtn.forEach(element => {
+        element.addEventListener('click', switchModal);
+    });
+
+    closeBtn, addEventListener('click', switchModal);
+
+});
+*/
+
+
+$(document).ready(function() {
+    var modal = $('.modal'),
+        modalBtn = $('[data-toggle=modal]'),
+        closelBtn = $('.modal__close');
+
+    modalBtn.on('click', function() {
+        modal.toggleClass('modal--visible');
+    });
+    closeBtn.on('click', function() {
+        modal.toggleClass('modal--visible');
+    });
+});
+
+$(document).ready(function() {
+    //initialize swiper when document ready
+    var mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        keyboard: {
+            enabled: true,
+            onlyInViewport: false,
+        },
+    });
+
+    var next = $('.swiper-button-next');
+    var prev = $('.swiper-button-prev');
+    var bullets = $('.swiper-pagination');
+
+    next.css('left', prev.width() + 120);
+    prev.css('right', next.width() + 120);
+
+});

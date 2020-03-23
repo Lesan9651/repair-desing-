8cc7aa20dea5a9665fbbb69942927f317791badf
Alexander Kirienkov <shurakirienkov@yandex.ru>
2020-03-23 20:51:53 +0300

    feat: gulp-clean, minify, htmlmin, tinypng-compress

--- gulpfile.js
+++ gulpfile.js
@@ -73,8 +73,8 @@ function fonts(done) {
 
 function imagemin(done) {
     src('img/**/**')
-        .pipe(tinypng({ key: 'API_KEY' })) /
-        .pipe('dist/img/')
+        .pipe(tinypng({ key: 'API_KEY' }))
+        .pipe(dest('dist/img/'))
     src('img/**/*.svg')
         .pipe(dest('dist/img/'))
     done();
